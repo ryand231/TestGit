60eb8bd4ad1f601363f8759cadec77eacd02e867
ryand231 <ryandwyer231@gmail.com>
2020-05-04 20:53:23 -0500

    This is commit from Atom

--- test_python.py
+++ test_python.py
@@ -3,3 +3,10 @@ print('Hello World')
 print('This is going to be an amazing build')
 
 print('Now we begin')
+
+startnum = 0
+listvar = [2, 4, 1, 8]
+for num in listvar:
+    startnum = startnum+num
+    print(startnum)
+print(startnum)
